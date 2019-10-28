2f1192924c55f70abcb7d2b3e9a0d33693341ab6
Sérgio Miguel <thesergiomiguel@gmail.com>
2019-10-28 11:30:03 +0100

    Format rules/typescript.js

--- rules/typescript.js
+++ rules/typescript.js
@@ -1,20 +1,29 @@
 module.exports = {
   rules: {
-    '@typescript-eslint/camelcase': ['error', {
-      properties: 'never',
-    }],
+    '@typescript-eslint/camelcase': [
+      'error',
+      {
+        properties: 'never',
+      },
+    ],
 
     '@typescript-eslint/explicit-function-return-type': 'off',
 
-    '@typescript-eslint/explicit-member-accessibility': ['warn', {
-      accessibility: 'no-public',
-      overrides: {
-        constructors: 'off',
-      }
-    }],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'warn',
+      {
+        accessibility: 'no-public',
+        overrides: {
+          constructors: 'off',
+        },
+      },
+    ],
 
-    '@typescript-eslint/no-unused-vars': ['error', {
-      'args': 'after-used'
-    }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+      },
+    ],
   },
 };
