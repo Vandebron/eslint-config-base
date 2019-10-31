8937d3da59a2d73ca85a06dbabdfa9140be5e68b
Sérgio Miguel <thesergiomiguel@gmail.com>
2019-10-31 16:41:03 +0100

    Extend prettier & prettier/react config

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
     'prettier/@typescript-eslint',
+    'prettier/react',
     'plugin:prettier/recommended',
     'plugin:import/typescript',
 
